e33712a6c5a3950dc77947d42b71034529954e2b
Kliuev Dmitrii <dimik1986@gmail.com>
2025-02-20 12:06:13 +0400

    #

---
+++ pages/AuthPage.py
@@ -0,0 +1,4 @@
+class AuthPage:
+
+    def __init__(self) -> None:
+        self.url = "https://id.atlassian.com/login?application=trello&continue=https%3A%2F%2Ftrello.com%2Fauth%2Fatlassian%2Fcallback%3FreturnUrl%3D%252Fu%252Fkliuev_dmitrii%252Fboards%26display%3D%26aaOnboarding%3D%26updateEmail%3D%26traceId%3D%26ssoVerified%3D%26createMember%3D%26jiraInviteLink%3D"
