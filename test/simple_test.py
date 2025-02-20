b2286339292c51497338e89657475beb43a13328
Kliuev Dmitrii <dimik1986@gmail.com>
2025-02-20 13:22:59 +0400

    #

--- test/simple_test.py
+++ test/simple_test.py
@@ -2,13 +2,14 @@ import time
 from selenium.webdriver.remote.webdriver import WebDriver 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager 
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
-from pages.AuthPage import AuthPage
+from page.AuthPage import AuthPage
 
 def first_test():
     browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    browser.implicity_wait(4)
+    browser.implicitly_wait(4)
     browser.maximize_window()
 
     auth_page = AuthPage(browser)
