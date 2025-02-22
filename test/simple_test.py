a5d12ac50a402a0cfb15c18371f26d0906fb0976
Kliuev Dmitrii <dimik1986@gmail.com>
2025-02-22 19:05:21 +0400

    #

--- test/simple_test.py
+++ test/simple_test.py
@@ -1,9 +1,13 @@
 from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC
+
 from pytest_ui_api_template.pages.AuthPage import AuthPage
 
 def auth_test(browser):
     auth_page = AuthPage(browser)
     auth_page.go()
     auth_page.login_as("dimik1986@gmail.com", "di19ma86K@")
-    sleep(10)
+    WebDriverWait(browser, 15).until(EC.url_contains("kliuev_dmitrii/boards"))
+    
     assert auth_page.get_current_url().endswith("kliuev_dmitrii/boards")
